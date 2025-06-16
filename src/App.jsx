2c54ef438edb4634bbfd7fad0fadc29e908666de
unknown <elviana23ti@mahasiswa.pcr.ac.id>
2025-06-16 09:11:15 +0700

    belum edit admin

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, { Suspense } from "react";
+import './assets/tailwind.css';
+import { Route, Routes } from "react-router-dom";
+import Admin from "./pages/Admin/Admin";
+
+
+const Guest = React.lazy(() => import("./pages/Guest/Guest"))
+const Loading = React.lazy(() => import("./components/Guest/Loading"));
+const GuestLayout = React.lazy(() => import("./layouts/GuestLayout"));
+const AuthLayout = React.lazy(() => import("./layouts/AuthLayout"));
+const AdminLayout = React.lazy(() => import("./layouts/AdminLayout"));
+const Login = React.lazy(() => import("./pages/auth/Login"));
+const Register = React.lazy(() => import("./pages/auth/Register"));
+const Forgot = React.lazy(() => import("./pages/auth/Forgot"));
+
+;function App() {
+  return (
+    <Suspense fallback={<Loading />}>
+      <Routes>
+        <Route element={<GuestLayout />}>
+          <Route path="/" element={<Guest />} />
+        </Route>
+
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot" element={<Forgot />} />
+        </Route>
+
+        <Route element={<AdminLayout />}>
+          <Route path="/admin" element={<Admin />} />
+        </Route>
+        
+      </Routes>
+    </Suspense>
+  );
+}
+
+export default App;
---
+++ src/components/Admin/HeaderAdmin.jsx
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export default function HeaderAdmin() {
+  const location = useLocation();
+  const [activeSection, setActiveSection] = useState("");
+
+  const handleScroll = () => {
+    const sections = ["#about", "#topProduk", "#testi", "#kontak"];
+    const sectionInView = sections.find((section) => {
+      const element = document.querySelector(section);
+      const rect = element?.getBoundingClientRect();
+      return rect && rect.top <= 0 && rect.bottom > 0;
+    });
+    setActiveSection(sectionInView);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <header className="fixed w-full top-0 z-50 bg-white">
+      <div className="container max-w-screen-xl mx-auto px-6 flex items-center justify-between py-5">
+        <a
+          href="/admin"
+          className="font-poppins-extrabold text-[40px] text-gray-900"
+        >
+          Drive<span className="text-cyan-300">X</span>
+        </a>
+
+        <nav className="flex space-x-9 pl-50">
+          <a
+            href="/admin"
+            className={`${
+              location.pathname === "/admin" && activeSection === ""
+                ? "text-orange-500"
+                : "text-gray-800"
+            } hover:text-orange-500 font-RethinkSans-SemiBold text-base tracking-wide`}
+          >
+            Home
+          </a>
+          <a
+            href="/admin#about"
+            className={`${
+              activeSection === "#about" ? "text-orange-500" : "text-gray-800"
+            } hover:text-orange-500 font-RethinkSans-SemiBold text-base tracking-wide`}
+          >
+            About
+          </a>
+          <a
+            href="/admin#topProduk"
+            className={`${
+              activeSection === "#topProduk" ? "text-orange-500" : "text-gray-800"
+            } hover:text-orange-500 font-RethinkSans-SemiBold text-base tracking-wide`}
+          >
+            Produk
+          </a>
+          <a
+            href="/cekProduk"
+            className={`${
+              location.pathname === "/cekProduk" ? "text-orange-500" : "text-gray-800"
+            } hover:text-orange-500 font-RethinkSans-SemiBold text-base tracking-wide`}
+          >
+            Cek Stok
+          </a>
+
+          <a
+            href="/admin#testi"
+            className={`${
+              activeSection === "#testi" ? "text-orange-500" : "text-gray-800"
+            } hover:text-orange-500 font-RethinkSans-SemiBold text-base tracking-wide`}
+          >
+            Testimoni
+          </a>
+          <a
+            href="/admin#kontak"
+            className={`${
+              activeSection === "#kontak" ? "text-orange-500" : "text-gray-800"
+            } hover:text-orange-500 font-RethinkSans-SemiBold text-base tracking-wide`}
+          >
+            Kontak
+          </a>
+        </nav>
+
+        <div className="flex items-center space-x-5 ml-10">
+        <a
+            href="/"
+            className="px-6 py-2 bg-transparent border-2 border-orange-500 text-orange-500 text-sm font-RethinkSans-SemiBold rounded-full hover:bg-orange-500 hover:text-white transition duration-300"
+          >
+            Logout
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+}
